de9bb09f1d02d744cd682029ed59b9a664be6e7e
aquadelight <apooyin95@gmail.com>
2022-09-08 11:15:57 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,6 +7,8 @@
  */
 int main(void)
 {
+	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
