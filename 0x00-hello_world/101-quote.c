14f3a48a2ad65888a473f271c34cce962ccf1b08
aquadelight <apooyin95@gmail.com>
2022-09-08 17:30:14 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-* main-prints the standard error sandwiched around the standard output
-* Return:returns 1
-*/
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
 	write(2,"and that piece of art is useful\",-Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	return(1);
 }
