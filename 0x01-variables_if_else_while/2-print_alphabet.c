56d4db24449a0ae8f6c37d53cc245b992c5493e3
aquadelight <apooyin95@gmail.com>
2022-09-09 21:05:58 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,16 +4,17 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
- int main(void)
- {
- 	char c = 'a';
+ * */
+int main(void)
+{
+	char c = 'a';
 
 	while (c <= 'z')
 	{
-	putchar(c);
-	c++;
-	}	
+		putchar(c);
+		c++;
+
+	}
 	putchar('\n');
 
 	return (0);
