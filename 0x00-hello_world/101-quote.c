2990d2c19f1a39d7388131b55b7e9864907fd7b5
aquadelight <apooyin95@gmail.com>
2022-09-12 13:12:49 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include<unistd.h>
+#include <unistd.h>
+
 
 /**
  * main - Prints"and that piece of art is useful"-Dora Korpar, 2015-10-19",
@@ -10,5 +11,5 @@ int main(void)
 {
  write(2,
 	"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
- return(1);
+ return (1);
 }
