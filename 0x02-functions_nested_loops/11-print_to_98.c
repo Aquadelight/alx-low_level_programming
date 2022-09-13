92035db1b513d6ced55500cdcde2f1ace31db0be
aquadelight <apooyin95@gmail.com>
2022-09-13 12:52:58 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- *print_to_98 - prints all natural numbers from n to 98
+ * print_to_98 - prints all natural numbers from n to 98
  * @n: input number to check
  * Return: nothing
  */
