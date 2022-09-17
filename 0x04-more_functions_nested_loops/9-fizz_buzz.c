6d711c301ced633e066503ef6c02d86179961aaa
aquadelight <apooyin95@gmail.com>
2022-09-16 17:53:19 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,15 @@ int main(void)
 	printf("%d", a);
 	while (i < 101)
 	{
-		if (i % 3 == 0 i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf(" FizzBuzz");
-		else if (i % 3 = 0)
-			printf" Fizz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
 		else if (i % 5 == 0)
 			printf(" Buzz");
 		else
 			printf(" %d", i);
-		i++,
+		i++;
 	}
 	printf("\n");
 	return (0);
