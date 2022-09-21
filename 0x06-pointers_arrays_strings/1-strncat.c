ffc0ddb7ba824faada0bd8d0e0d3740264e8b9df
aquadelight <apooyin95@gmail.com>
2022-09-21 11:28:53 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,7 +19,8 @@ char *_strncat(char *dest, char *src, int n)
 	while (!(*(src + j) == '\0' || j == n))
 	{
 		*(dest + i) = *(src + j);
-		i++;										j++;
+		i++;
+		j++;
 	}
 	*(dest + i) = '\0';
 	p = dest;
